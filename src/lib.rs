67ddb46e85bc5bfad6cede9610e3c59f97fa66f7
Steve Kane <kanesteven@gmail.com>
2015-10-31 22:24:39 -0500

    working example of the bind system I'd like to macroify....but choice is no bueno due to iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,27 @@
 use std::fmt;
 
-pub fn is_alpha(b: u8) -> bool { (b as char).is_alphabetic() }
-pub fn is_digit(b: u8) -> bool { (b as char).is_digit(10) }
-
-pub enum Reply<I:Iterator, O> {
+pub enum Reply<I:Iterator<Item=u8>, O> {
     Ok(O, I),
-    Err
+    Err(I)
 }
 
-pub enum Consumed<I:Iterator, O> {
+pub enum Consumed<I:Iterator<Item=u8>, O> {
     Consumed(Reply<I, O>),
     Empty(Reply<I, O>),
 }
 
-impl <I:Iterator, O:fmt::Debug> fmt::Debug for Reply<I, O> {
+pub type Parser<I, O> = Fn(I) -> Consumed<I, O>;
+
+impl <I:Iterator<Item=u8>, O:fmt::Debug> fmt::Debug for Reply<I, O> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             &Reply::Ok(ref t, _) => write!(f, "Ok({:?}, Iterator)", t),
-            &Reply::Err          => write!(f, "Err"),
+            &Reply::Err(_)       => write!(f, "Err"),
         } 
     }
 }
 
-impl <I:Iterator, O:fmt::Debug> fmt::Debug for Consumed<I, O> {
+impl <I:Iterator<Item=u8>, O:fmt::Debug> fmt::Debug for Consumed<I, O> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             &Consumed::Consumed(ref reply) => write!(f, "Consumed({:?})", reply),
@@ -31,98 +30,98 @@ impl <I:Iterator, O:fmt::Debug> fmt::Debug for Consumed<I, O> {
     }
 }
 
-pub fn unit<I, O> (t: O, input: I) -> Consumed<I, O> 
-    where I:Iterator {
-    use Consumed::Consumed;
-    use Reply::Ok;
+#[inline]
+pub fn is_alpha(b: u8) -> bool { (b as char).is_alphabetic() }
+#[inline]
+pub fn is_digit(b: u8) -> bool { (b as char).is_digit(10) }
 
-    Consumed(Ok(t, input)) 
-}
-
-pub fn satisfy<I, F> (f: F, mut input: I) -> Consumed<I, I::Item>
-    where I:Iterator,
-          I::Item:Copy,
-          F:FnOnce(I::Item) -> bool {
-    use Consumed::{Empty, Consumed};
-    use Reply::{Ok, Err};
+pub fn digit<I>(mut input: I) -> Consumed<I, u8> 
+    where I:Iterator<Item=u8> {
 
     match input.next() {
-        None    => Empty(Err),
-        Some(b) => match f(b) {
-            true  => Consumed(Ok(b, input)),
-            false => Empty(Err),
+        None    => Consumed::Empty(Reply::Err(input)),
+        Some(b) => match (b as char).is_digit(10) {
+            true  => Consumed::Consumed(Reply::Ok(b, input)),
+            false => Consumed::Empty(Reply::Err(input)),
         }
     }
 }
 
-pub fn digit<I>(mut input: I) -> Consumed<I, u8> {
+pub fn char<I>(mut input: I) -> Consumed<I, u8>
+    where I:Iterator<Item=u8> {
+
     match input.next() {
-        None    => Empty(Err),
-        Some(b) => match is_digit(b) {
-            true  => Consumed(Ok(b, input)),
-            false => Empty(Err),
+        None    => Consumed::Empty(Reply::Err(input)),
+        Some(b) => match (b as char).is_alphabetic() {
+            true  => Consumed::Consumed(Reply::Ok(b, input)),
+            false => Consumed::Empty(Reply::Err(input)),
         }
     }
 }
 
-pub fn bind<I, O, T, F>(c: Consumed<I, O>, f: F) -> Consumed<I, T>
-    where I:Iterator,
-          F:FnOnce(O, I) -> Consumed<I, T> {
-    use Consumed::{Empty, Consumed};
-    use Reply::{Ok, Err};
+impl <I:Iterator<Item=u8>, A> Consumed<I, A> {
+    pub fn bind<F, B> (self, f: F) -> Consumed<I, B>
+        where F:FnOnce(A, I) -> Consumed<I, B> {
+        
+        use Consumed::{Empty, Consumed};
+        use Reply::{Ok, Err};
 
-    match c {
-        Empty(reply) => match reply {
-            Ok(o, i) => f(o, i),
-            Err      => Empty(Err),
-        },
-        Consumed(reply) => Consumed(match reply {
-            Ok(o, i) => match f(o, i) {
-                Consumed(reply2) => reply2,
-                Empty(reply2)    => reply2,
+        match self {
+            Empty(reply) => match reply {
+                Ok(o, i) => f(o, i),
+                Err(i)   => Empty(Err(i)),
             },
-            Err      => Err,
-        })
-    } 
+            Consumed(reply) => Consumed(match reply {
+                Ok(o, i) => match f(o, i) {
+                    Consumed(reply2) => reply2,
+                    Empty(reply2)    => reply2,
+                },
+                Err(i)  => Err(i)
+            })
+        } 
+    }
+
+    //TODO: consider switching to consume a fixed buffer instead of iterator
+    //this solves some complexities with look ahead on iterators
+    pub fn choice<F> (self, f:F) -> Consumed<I, A>
+        where F:FnOnce(I) -> Consumed<I, A> {
+        
+        use Consumed::Empty;
+        use Reply::{Ok, Err};
+
+        match self {
+            Empty(Err(i))   => f(i),
+            Empty(Ok(o, i)) => match f(i) {
+                Empty(Ok(_, i)) => Empty(Ok(o, i)),
+                consumed @ _ => consumed,
+            },
+            consumed @ _ => consumed
+        }
+    }
 }
 
-//pub fn choice<I, O, F>(c: Consumed<I, O>, f:F) -> Consumed<I, O>
-//    where I:Iterator,
-//          F:FnOnce(O, I) -> Consumed<I, O> {
-//    use Consumed::{Empty, Consumed};
-//    use Reply::{Ok, Err};
-//
-//    match c {
-//        Empty(Err) => f
-//    }
-//}
+// parser! first_two<I:Iterator<Item=u8>, O> {
+//     d1 <- digit
+//     d2 <- digit
+//     return (d1, d2)
+// }
 
-#[test]
-fn test_unit() {
-    let string = String::from("asfkja1231241");
-    let bytes = string.bytes();
-    let result = unit('a', bytes);
-
-    println!("{:?}", result);
+fn first_two<I:Iterator<Item=u8>> (i: I) -> Consumed<I, (u8, u8)> {
+    digit(i).bind(|d1, i|
+    digit(i).bind(|d2, i| 
+    Consumed::Consumed(Reply::Ok((d1, d2), i))))
 }
 
-#[test]
-fn test_satisfy() {
-    let string = String::from("asfkja1231241");
-    let bytes = string.bytes();
-    let result = satisfy(is_alpha, bytes);
+#[cfg(test)]
+mod tests {
+    use super::first_two;
 
-    println!("{:?}", result);
-}
+    #[test]
+    fn test_do_block() {
+        let string = String::from("123145skfjhalb1");
+        let bytes = string.bytes();
+        let result = first_two(bytes);
 
-#[test]
-fn test_bind() {
-    let string = String::from("asfkja1231241");
-    let bytes = string.bytes();
-    let result = bind(satisfy(is_alpha, bytes), |o, i| {
-                 bind(satisfy(is_alpha, i), |o2, i| {
-                 bind(satisfy(|c| c as char == 'f', i), |o3, i| {
-                 Consumed::Consumed(Reply::Ok(vec![o, o2, o3], i))})})});
-
-    println!("{:?}", result);
+        println!("{:?}", result);
+    }
 }
